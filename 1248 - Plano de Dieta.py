761d0153e9d017e95ffca23c9f3ded72ac4786a5
Jackson Souza <73433159+JacksonSouz4@users.noreply.github.com>
2021-12-09 11:37:29 -0300

    Update 1248 - Plano de Dieta.py

--- 1248 - Plano de Dieta.py
+++ 1248 - Plano de Dieta.py
@@ -1,31 +1,22 @@
 entrada = int(input())
 for c in range(entrada):
-    dietaLista = list()
-    cafeLista = list()
-    almocoLista = list()
-    dieta = input()
-    cafe = input()
-    almoco = input()
-    for comida in dieta:
-        dietaLista.append(comida)
-    for comida in cafe:
-        cafeLista.append(comida)
-    for comida in almoco:
-        almocoLista.append(comida)
+    dieta = list(input())
+    cafe = list(input())
+    almoco = list(input())
     erros = 0
-    for comida in cafeLista:
-        if comida in dietaLista:
-            dietaLista.remove(comida)
+    for comida in cafe:
+        if comida in dieta:
+            dieta.remove(comida)
         else:
             erros += 1
-    for comida in almocoLista:
-        if comida in dietaLista:
-            dietaLista.remove(comida)
+    for comida in almoco:
+        if comida in dieta:
+            dieta.remove(comida)
         else:
             erros += 1
     if erros == 0:
-        for comida in sorted(dietaLista):
-            if len(dietaLista) > 0:
+        for comida in sorted(dieta):
+            if len(dieta) > 0:
                 print(f'{comida}', end='')
         print()
     else:
