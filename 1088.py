0bc10bd3af8328d988ccecca03759a20856cd810
souzajackson <73433159+souzajackson@users.noreply.github.com>
2022-02-08 11:28:28 -0300

    Update 1088.py

--- 1088.py
+++ 1088.py
@@ -3,9 +3,7 @@ for entrada in stdin.readlines()[:-1]:
     seq = [int(n) - 1 for n in entrada.split()[1:]]
     ordenado = sorted(seq)
     jogadasFeitas = 0
-    while True:
-        if seq == ordenado:
-            break
+    while seq != ordenado:
         for atual, certo in zip(seq, ordenado):
             if atual != certo:
                 seq[atual], seq[certo] = seq[certo], seq[atual]
